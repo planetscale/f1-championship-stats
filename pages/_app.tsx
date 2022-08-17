226294ec6df5f5b96cae11be438b9cc9a2c347dd
Derek Briggs <derek.briggs@me.com>
2022-08-17 09:45:04 -0600

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [favicon, setFavicon] = useState('/favicon_system.svg?v=2')
+
+  useEffect(() => {
+    const onChange = (event) => {
+      setFavicon(`/favicon_${event.matches ? 'dark' : 'light'}.svg?v=2`)
+    }
+    const query = window.matchMedia('(prefers-color-scheme: dark)')
+    if (query.addEventListener) {
+      query.addEventListener('change', onChange)
+    }
+    return () => query.removeEventListener('change', onChange)
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <link rel='icon' href='/favicon.ico?v=2' sizes='any' />
+        <link rel='icon' href={favicon} sizes='any' type='image/svg+xml' />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
